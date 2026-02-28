6489d4e77d180c986ea97a56474d52cb8407b544
Patrick Marx <6949483+codesorcery@users.noreply.github.com>
2026-02-28 13:04:23 +0100

    fix: use correct valid_until instead of erroneous valid_to

--- sync.py
+++ sync.py
@@ -64,7 +64,7 @@ class Member:
                 "family_name": self.family_name,
             },
             "valid_from": "20260101",
-            "valid_to": "29991231",
+            "valid_until": "29991231",
         }
 
 
